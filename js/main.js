9e80b6d2344aa7654dbc8f5202d0a6ac8f0a36da
TheSchrodingerCat <mariaj.pozom@gmail.com>
2017-06-18 18:15:50 -0400

    se crea funcion para guardar datos y se comienza a crear el contenedor de comentarios

--- js/main.js
+++ js/main.js
@@ -3,4 +3,24 @@
 
 	//botones se crean en el index.html
 
+	//llamamos a la ID del botón Guardar y le asignamos el evento "click"
+	document.getElementById("guardar").addEventListener("click",postear);
+
+	function postear(){
+
+		//primero, se guardan los datos
+		(function guardarDatos(){
+			localStorage.nombre = document.getElementById("clave").value;
+			localStorage.comentario = document.getElementById("valor").value;
+		})();
+
+
+
+		//lamamos a la ID llamamos a la ID de el div contenedor
+		var contenedor = document.getElementById("mis-comentarios");
+
+		//creamos la div del comentario
+		var cajaComentario = document.createElement("div");
+		cajaComentario.classList.add("new-coment");
+	}
 })();
